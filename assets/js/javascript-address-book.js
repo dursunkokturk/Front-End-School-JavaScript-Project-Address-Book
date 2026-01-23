75b02ed81efb5f89c0e6e9a0384f1bd493864a10
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-01-23 10:20:00 +0300

    assets klasoru icindeki js klasoru icinde yer alan javascript-address-book.js dosyasinda senaryo olusturuldu

---
+++ assets/js/javascript-address-book.js
@@ -0,0 +1,63 @@
+/*
+Adres Defteri
+
+Veri Yapılarını Oluşturma
+
+Aşağıdaki bilgiler için ayrı ayrı diziler (array) oluşturunuz:
+
+    Ad, Soyad, Adres, Telefon, E-posta
+
+Kullanıcı Menüsü
+
+Kullanıcıya bir menü gösteriniz.
+
+    Menüde en az şu seçenekler bulunmalıdır:
+
+1 - Kayıt Ekle
+
+2 - Çıkış
+
+
+Kayıt Ekleme
+
+Kullanıcı 1 seçeneğini seçtiğinde:
+
+    Ad, Soyad, Adres, Telefon, E-posta
+
+bilgilerini prompt kullanarak alınız. Alınan her bilgiyi ilgili dizinin sonuna (push) ekleyiniz.
+
+
+Tekrar Menüye Dönme
+
+    Kayıt eklendikten sonra kullanıcı tekrar menüye yönlendirilmelidir.
+    Kullanıcı 2 - Çıkış seçeneğini seçene kadar program çalışmaya devam etmelidir.
+
+HTML’e Yazdırma
+
+Dizilerde tutulan kayıtları HTML üzerinde liste halinde gösteriniz.
+
+Her kayıt için:
+
+    Ad + Soyad
+    Adres
+    Telefon
+    E-posta
+
+bilgileri okunabilir şekilde yer almalıdır.
+
+
+Stil (CSS)
+
+Sayfanın görsel tasarımı tamamen size bırakılmıştır.
+
+Liste yapısı, yazı tipi, renkler ve düzen konusunda özgürsünüz.
+
+
+Vakti kalanlar için:
+
+Kart tasarımı
+
+Hover efektleri
+
+Flex veya Grid kullanabilirsiniz.
+*/
